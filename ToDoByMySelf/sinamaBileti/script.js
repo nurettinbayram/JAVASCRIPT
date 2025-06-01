c079dd74687b2262409f231ca689d9f08e3cc61e
nurettinBayram <fakhannurettin@gmail.com>
2025-05-31 23:10:20 -0400

    sinama bileti ve hesap makinasi

--- ToDoByMySelf/sinamaBileti/script.js
+++ ToDoByMySelf/sinamaBileti/script.js
@@ -1,10 +1,15 @@
 const container = document.querySelector(".container");
+const allSeats = document.querySelectorAll(".seat");
 const seats = document.querySelectorAll(".seat:not(reserved)");
 const amount = document.getElementById("amount");
 const option = document.getElementById("movies");
+const reservedBtn = document.getElementById("reservedBtn");
+var reserved = document.querySelectorAll(".seat.reserved");
+const unReservedBtn = document.getElementById("unReservedBtn");
 
 getFromLocalStorage();
 culculateTicet();
+getLocalStorageReseveIndex();
 
 container.addEventListener("click", function (e) {
   let myTarget = e.target;
@@ -15,6 +20,10 @@ container.addEventListener("click", function (e) {
   ) {
     myTarget.classList.toggle("selected");
     culculateTicet();
+  }
+
+  if (myTarget.classList.contains("unReserved")) {
+    myTarget.classList.remove("reserved", "unReserved");
   }
 });
 
@@ -44,7 +53,6 @@ function culculateTicet() {
   const selecedSeatsIndex = selecedSeatsArr.map(function (seat) {
     return seatsArr.indexOf(seat);
   });
-  console.log(selecedSeatsIndex);
 
   saveToLocalStorge(selecedSeatsIndex);
 }
@@ -77,3 +85,58 @@ function getFromLocalStorage() {
     option.selectedIndex = selectedMovieLocalStorege; //option.selectedIndex etiketine ait ozellik
   }
 }
+
+//? burada reserve butonuna basilinca secili olan koltular reseve edilir.
+reservedBtn.addEventListener("click", function () {
+  selectedSeats = container.querySelectorAll(".seat.selected");
+  selectedSeats.forEach(function (seat) {
+    seat.classList.remove("selected");
+    seat.classList.add("reserved");
+  });
+  reserved = document.querySelectorAll(".seat.reserved");
+
+  let reserevUnReserve = document.querySelectorAll(".reserved.unReserved");
+  reserevUnReserve.forEach((item) => {
+    item.classList.remove("unReserved");
+  });
+
+  let reservedArr = [];
+  let allSeatArr = [];
+  allSeats.forEach(function (res) {
+    allSeatArr.push(res);
+    if (res.classList.contains("reserved")) {
+      reservedArr.push(res);
+    }
+  });
+
+  let reservedIndex = reservedArr.map(function (reserve) {
+    return allSeatArr.indexOf(reserve);
+  });
+
+  setLocalStorageReseveIndex(reservedIndex);
+
+  culculateTicet();
+});
+
+function setLocalStorageReseveIndex(reservedIndex) {
+  localStorage.setItem("reservedIndex", JSON.stringify(reservedIndex));
+}
+
+function getLocalStorageReseveIndex() {
+  let reservedIndex = JSON.parse(localStorage.getItem("reservedIndex"));
+
+  if (reservedIndex != null && reservedIndex.length > 0) {
+    allSeats.forEach(function (item, index) {
+      if (reservedIndex.indexOf(index) > -1) {
+        item.classList.add("reserved");
+      }
+    });
+  }
+}
+
+unReservedBtn.addEventListener("click", function () {
+  reserved = document.querySelectorAll(".seat.reserved");
+  reserved.forEach((item) => {
+    item.classList.add("unReserved");
+  });
+});
